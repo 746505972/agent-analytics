cccf73a3c3492006d1de22244509bff28eeb6687
746505972 <746505972@qq.com>
2025-11-26 15:58:19 +0800

    修改徽章

--- frontend/src/views/Upload.vue
+++ frontend/src/views/Upload.vue
@@ -5,11 +5,6 @@
         <div class="header-left">
           <a href="https://github.com/746505972/agent-analytics" target="_blank" class="github-link">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" width="20" alt="GitHub">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" width="20" alt="NodeJS">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original.svg" width="20" alt="npm">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg" width="20" alt="numpy">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ngrok/ngrok-original.svg" width="20" alt="ngrok">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg" width="20" alt="nginx">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg" width="20" alt="pandas">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" width="20" alt="python">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg" width="20" alt="pytorch">
